ed4446bb71b8d787180b0afe7ccef11aa96d9bde
aidanliuheng <aidanliuheng@didiglobal.com>
2022-07-15 08:48:51 +0800

    160

--- LinkList/160_intersection_of_two_linked_lists.cpp
+++ LinkList/160_intersection_of_two_linked_lists.cpp
@@ -65,4 +65,15 @@ class Solution {
         }
         return NULL;
     }
+    // 使用双指针进行遍历 v2
+    ListNode* getIntersectionNode_v2(ListNode* headA, ListNode* headB) {
+        if (headA == NULL || headB == NULL) return NULL;
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 == NULL ? headB : p1->next;
+            p2 = p2 == NULL ? headA : p2->next;
+        }
+        return p1;
+    }
 };
